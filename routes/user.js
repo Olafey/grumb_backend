4a84077d0bac02f7a310aff2a5247d158696a356
Olafey <olamidearemu2000@yahoo.com>
2025-03-17 09:46:58 +0100

    routes invoked

--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,11 @@
 const express = require("express");
-const userRouter = express.Router;
+const userRouter = express.Router();
+
+userRouter.post("/", (req, res) => {});
+userRouter.get("/", (req, res) => {
+  res.json({
+    message: "Get User",
+  });
+});
 
 module.exports = userRouter;
